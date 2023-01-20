86a0facef69cb97c7de799d235382c749944f137
Marcus Paulo <marcuscarvalho0608@gmail.com>
2023-01-20 15:11:22 -0300

    projeto_habits_part_2

--- app_habito/server/src/routes.ts
+++ app_habito/server/src/routes.ts
@@ -6,7 +6,7 @@ import dayjs from "dayjs"
 
 
 export async function appRoutes(app: FastifyInstance){
-  app.get('/habits', async(request) => {
+  app.post('/habits', async (request) => {
     const createHabitBody = z.object({
       title: z.string(),
       weekDays: z.array(
@@ -37,6 +37,42 @@ export async function appRoutes(app: FastifyInstance){
     const getDayParams = z.object({
       date: z.coerce.date()
     })
+
+    const { date } = getDayParams.parse(request.query)
+
+    const parsedDate = dayjs(date).startOf('day')
+    const weekDay = parsedDate.get('day')
+
+    const possibleHabits = await prisma.habit
+    .findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true
+      }
+    })
+
+    const completedHabit = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id
+    })
+    return {
+      possibleHabits,
+      completedHabit
+    }
   })
 
 }
